cdf77222a7843526ee7ac28c9bf3886afc26a7bd
Collin Van Dyck <collinvandyck@gmail.com>
2013-03-26 18:24:18 -0700

    Minor code rearranging

--- gesture.go
+++ gesture.go
@@ -15,10 +15,26 @@ var (
 	channels = []string{"#collinjester"}
 )
 
-// when an error occurs, calling this method will send the error back to the irc channel
-func sendError(conn *irc.Conn, channel string, nick string, err error) {
-	log.Print(err)
-	conn.Privmsg(channel, fmt.Sprintf("%s: oops: %v", nick, err))
+func main() {
+	flag.Parse()
+	c := irc.SimpleClient("gesturebot")
+	c.SSL = true
+	c.AddHandler(irc.CONNECTED,
+		func(conn *irc.Conn, line *irc.Line) {
+			for _, channel := range channels {
+				conn.Join(channel)
+			}
+		})
+	quit := make(chan bool)
+	c.AddHandler(irc.DISCONNECTED, func(conn *irc.Conn, line *irc.Line) { quit <- true })
+	c.AddHandler("PRIVMSG", func(conn *irc.Conn, line *irc.Line) {
+		messageReceived(conn, line)
+	})
+	if err := c.Connect("irc.freenode.net"); err != nil {
+		fmt.Printf("Connection error: %s\n", err)
+	}
+	// Wait for disconnect
+	<-quit
 }
 
 // When a message comes in on a channel gesture has joined, this method will be called.
@@ -63,24 +79,9 @@ func messageReceived(conn *irc.Conn, line *irc.Line) {
 	}
 }
 
-func main() {
-	flag.Parse()
-	c := irc.SimpleClient("gesturebot")
-	c.SSL = true
-	c.AddHandler(irc.CONNECTED,
-		func(conn *irc.Conn, line *irc.Line) {
-			for _, channel := range channels {
-				conn.Join(channel)
-			}
-		})
-	quit := make(chan bool)
-	c.AddHandler(irc.DISCONNECTED, func(conn *irc.Conn, line *irc.Line) { quit <- true })
-	c.AddHandler("PRIVMSG", func(conn *irc.Conn, line *irc.Line) {
-		messageReceived(conn, line)
-	})
-	if err := c.Connect("irc.freenode.net"); err != nil {
-		fmt.Printf("Connection error: %s\n", err)
-	}
-	// Wait for disconnect
-	<-quit
+// when an error occurs, calling this method will send the error back to the irc channel
+func sendError(conn *irc.Conn, channel string, nick string, err error) {
+	log.Print(err)
+	conn.Privmsg(channel, fmt.Sprintf("%s: oops: %v", nick, err))
 }
+
