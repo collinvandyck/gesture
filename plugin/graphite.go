49ec3decd72df8cbbc91c8060966446b3e5dfaa5
Ben Linsay <benl@boundary.com>
2013-03-31 19:27:07 -0700

    Cleaned up Graphite

--- plugin/graphite.go
+++ plugin/graphite.go
@@ -1,18 +1,27 @@
 // appends png suffixes to graphite urls  so they render in the irc clients
-package graphite
+package plugin 
 
 import (
+	"errors"
 	"fmt"
 	"gesture/core"
-	"log"
 	"strings"
 )
 
-func Create(bot *core.Gobot) {
+func init() {
+	core.Register(Graphite{})
+}
+
+type Graphite struct{}
+
+func (g Graphite) Name() string {
+	return "graphite"
+}
+
+func (g Graphite) Create(bot *core.Gobot) error {
 	prefix, found := bot.Config.Plugins["graphite"]["prefix"].(string)
 	if !found {
-		log.Printf("Can't find graphite prefix!")
-		return
+		return errors.New("Can't find graphite prefix!")
 	}
 
 	pattern := fmt.Sprintf(`%s(\S+)`, prefix)
@@ -23,4 +32,6 @@ func Create(bot *core.Gobot) {
 		}
 		return nil
 	})
+
+	return nil
 }
